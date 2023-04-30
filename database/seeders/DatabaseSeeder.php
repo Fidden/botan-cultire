092442b9bdf6b82c979d36c0bdf03d328dbfe095
storm <sahalisa17@gmail.com>
2023-04-30 13:23:09 +0300

    feat: Добавлена базовая архитектура бека.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\BookScreenshot;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Book::factory(10)
+            ->create()
+            ->each(
+                fn(Book $book) => BookScreenshot::factory(5)->create(['book_id' => $book->id])
+            );
     }
 }
